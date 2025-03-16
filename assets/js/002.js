afb1a1a2abe3486f7539ef78ad9d7bd3b91d14d5
Alejandro <23019072+iamcapote@users.noreply.github.com>
2025-03-15 21:52:35 -0400

    Update 002.js

--- assets/js/002.js
+++ assets/js/002.js
@@ -1,51 +1,41 @@
-// By: h01000110 (hi)
-// github.com/h01000110
-
 function numbers () {
-	var fields = document.getElementsByTagName("code");
-	for (field in fields) {
-		var num = 0;
-		var select = fields[field].innerText;
-		var select_f = select.split(/\n/);
-		var tab = document.createElement("table");
+    var fields = document.getElementsByTagName("code");
+    for (var i = 0; i < fields.length; i++) {
+        var field = fields[i];
+        // Ensure the field has innerText (it should, if it's a <code> element)
+        if (field.innerText) {
+            var select = field.innerText;
+            var select_f = select.split(/\n/);
+            var tab = document.createElement("table");
 
-		// IF YOU USE MARKDOWN AND YOU HAVE BEEN GETTING ONE ADDITIONAL LINE IN YOUR TAG CODE
-		// UNCOMMENT THE SECTION BELOW
-		
-		/* MARKDOWN SECTION /**/
+            // If you need to remove an extra line added by markdown, uncomment:
+            // select_f.splice(-1, 1);
 
-		select_f.splice(-1, 1);
+            field.innerHTML = "";
+            field.appendChild(tab);
+            for (var j = 0; j < select_f.length; j++) {
+                var row = document.createElement("tr");
+                var col = document.createElement("th");
+                var colc = document.createElement("th");
+                col.innerText = j + 1;
+                colc.innerText = select_f[j];
+                row.appendChild(col);
+                row.appendChild(colc);
+                tab.appendChild(row);
 
-		/* END OF SECTION*/
-
-		fields[field].innerHTML = "";
-		fields[field].appendChild(tab);
-		for (line in select_f) {
-			var row = document.createElement("tr");
-			var col = document.createElement("th");
-			var colc = document.createElement("th");
-			col.innerText = num + 1;
-			colc.innerText = select_f[line];
-			row.appendChild(col);
-			row.appendChild(colc);
-			tab.appendChild(row);
-			num = num + 1;
-
-			// STYLE SECTION - If you want, change it below
-
-			col.style.textAlign = "right";
-			colc.style.textAlign = "left";
-			tab.style.border = "0";
-			col.style.border = "0";
-			colc.style.border = "0";
-			col.style.padding = "3px";
-			colc.style.padding = "3px";
-			col.style.borderRight = "2px solid #777777";
-
-			// END OF SECTION
-
-		}
-	}
+                // STYLE SECTION
+                col.style.textAlign = "right";
+                colc.style.textAlign = "left";
+                tab.style.border = "0";
+                col.style.border = "0";
+                colc.style.border = "0";
+                col.style.padding = "3px";
+                colc.style.padding = "3px";
+                col.style.borderRight = "2px solid #777777";
+            }
+        }
+    }
 }
 
-window.onload = numbers();
+// Set the onload event properly
+window.onload = numbers;
